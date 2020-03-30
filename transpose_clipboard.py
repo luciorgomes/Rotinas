fb48ec45361c8162d57c30080b870314e1d97d29
luciorgomes <luciorgomes@gmail.com>
2020-03-30 15:26:52 -0300

    Simplificação do procedimento

--- transpose_clipboard.py
+++ transpose_clipboard.py
@@ -8,15 +8,8 @@ import pyperclip
 def transpose_clipboard():
     mem = pyperclip.paste()
     mem = mem.split()
-    tranpos = ''
-    i = 0
-    for item in mem:
-        tranpos += item
-        if i < len(mem) - 1:
-            tranpos += ','
-        i += 1
-    print('Copiado para o clipboard')
-    pyperclip.copy(tranpos)
+    transposed = ','.join(mem)
+    pyperclip.copy(transposed)
 
 if __name__ == '__main__': # executa se chamado diretamente
     transpose_clipboard()
