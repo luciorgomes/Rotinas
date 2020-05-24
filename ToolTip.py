048007c9f4dda8d44e420fe07c75014518013237
luciorgomes <luciorgomes@gmail.com>
2020-05-24 00:17:43 -0300

    23/05/2020

--- ToolTip.py
+++ ToolTip.py
@@ -1,29 +1,43 @@
+'''
+Created on May 16, 2019
+Ch04
+@author: Burkhard A. Meier
+'''
+# ======================
+# imports
+# ======================
 import tkinter as tk
 
+
+# ===================================================================
 class ToolTip(object):
     def __init__(self, widget, tip_text=None):
         self.widget = widget
         self.tip_text = tip_text
-        widget.bind("<Enter>", self.mouse_enter) # o mouse entra no widget
-        widget.bind('<Leave>', self.mouse_leave) # o mouse sai do widget
-    
+        widget.bind('<Enter>', self.mouse_enter)  # bind mouse events
+        widget.bind('<Leave>', self.mouse_leave)
+
     def mouse_enter(self, _event):
         self.show_tooltip()
 
     def mouse_leave(self, _event):
-        self.hide_tootip()
+        self.hide_tooltip()
 
     def show_tooltip(self):
-        if self.tip_text: # se recebrr um texto...
-            x_left = self.widget.winfo_rootx()  # recebe a coordenada x do topo do widget
-            y_top = self.widget.winfo_rooty() - 18 # recebe a coordenada x do topo do widget e subtrai 25 para que apareça acima dele
-            self.tip_window = tk.Toplevel(self.widget) # cria a janela
-            self.tip_window.overrideredirect(True)  # remove a toolbar da janela criada
-            self.tip_window.geometry('+%d+%d'% (x_left, y_top)) # posiciona a jenela do tooptip
-            label = tk.Label(self.tip_window, text=self.tip_text, justify=tk.LEFT,  # label inserido na janela do Tooptip
-            background="#ffffe0", relief=tk.SOLID, borderwidth=1, font=("tahoma", '8', 'normal'), fg='black')
-            label.pack(ipadx=1) # torna o label visível
-    
-    def hide_tootip(self):
-        if self.tip_window: # se exite a jenela do Tooltip...
-            self.tip_window.destroy() 
+        if self.tip_text:
+            x_left = self.widget.winfo_rootx()  # get widget top-left coordinates
+            y_top = self.widget.winfo_rooty() - 18  # place tooltip above widget or it flickers
+
+            self.tip_window = tk.Toplevel(self.widget)  # create Toplevel window; parent=widget
+            self.tip_window.overrideredirect(True)  # remove surrounding toolbar window
+            self.tip_window.geometry("+%d+%d" % (x_left, y_top))  # position tooltip
+
+            label = tk.Label(self.tip_window, text=self.tip_text, justify=tk.LEFT,
+                             background="#ffffe0", relief=tk.SOLID, borderwidth=1, fg='black',
+                             font=("tahoma", "8", "normal"))
+            label.pack(ipadx=1)
+
+    def hide_tooltip(self):
+        if self.tip_window:
+            self.tip_window.destroy()       
+
